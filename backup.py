742658014d6b8827cb28dbd02a47afc6a7cb472b
BuildTools <unconfigured@null.spigotmc.org>
2023-08-29 01:56:20 +0000

    updated path

--- backup.py
+++ backup.py
@@ -2,5 +2,5 @@ import os
 from datetime import date
 today = date.today()
 
-os.chdir(/home/admin/minecraft)
+os.chdir("/home/admin/minecraft")
 os.system(f"git add . && git commit -m \"{today}\" && git push origin main")
